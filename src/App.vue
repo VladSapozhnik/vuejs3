20a72c4f24cca97383d4f467393f858ef95af735
Work <vladbars2@gmail.com>
2021-09-09 14:48:50 +0300

    fix start

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <main>
+    <TaskInput @onAddTask="addTask"></TaskInput>
+    <ul class="task-list my-list">
+      <li v-for="item in taskList" :key="item.id">
+        <TaskCard @onRemove="removeTask(item.id)" @onDone="setDoneTask(item.id)" :model="item"></TaskCard>
+      </li>
+    </ul>
+  </main>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import TaskInput from "./components/TaskInput.vue";
+import TaskCard from "./components/TaskCard.vue";
+import {ref} from 'vue'
+export default {
+  name: 'App',
+  components: {
+    TaskCard,
+    TaskInput
+  },
+  setup() {
+    const taskList = ref([{id: 0, title: 'Create video', description: 'And upload on YT', status: false}])
+    const addTask = ({title, description}) => {
+      taskList.value = [...taskList.value, {id: taskList.value[taskList.value.length - 1].id + 1, title, description, status: false}]
+    }
+    const setDoneTask = (id) => {
+      taskList.value = taskList.value.map(x => {
+        if(x.id === id)
+          x.status = true
+        return x
+      })
+    }
+    const removeTask = (id) => {
+      taskList.value = taskList.value.filter(x => x.id !== id)
+    }
+    return {
+      taskList,
+      addTask,
+      removeTask,
+      setDoneTask
+    }
+  }
 }
+</script>
+
+<style scoped>
+  .task-list {
+    list-style: none;
+  }
 </style>
